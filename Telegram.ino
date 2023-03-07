a6522b642b0c91cf805dae3d695246b9cafbb0d9
r0b0pic <atelier.robopic@gmail.com>
2023-03-07 18:54:29 +0100

    ajout des fichiers au dépôt

---
+++ Telegram.ino
@@ -0,0 +1,62 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/telegram-esp32-motion-detection-arduino/
+  
+  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+*/
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+// Replace with your network credentials
+const char* ssid = "*****";
+const char* password = "*****";
+
+// Initialize Telegram BOT
+#define BOTtoken "*****"  // your Bot Token (Get from Botfather)
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "*****"
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
+}
+
+void loop() {
+  
+    bot.sendMessage(CHAT_ID, "Hello Robopic!!", "");
+    Serial.println("Hello Robopic");
+    delay(5000);
+    bot.sendMessage(CHAT_ID, "DeepSleep!!", "");
+    Serial.println("DeepSleep");
+    esp_deep_sleep_start();
+  
+}
